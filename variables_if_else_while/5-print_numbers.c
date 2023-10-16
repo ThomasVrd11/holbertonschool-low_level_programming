bb3d7a37382f581199cd619a5c65ecfc5790803c
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-16 14:15:12 +0200

    ex 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main- on veut print de 0 a 10
+*
+*
+*
+*
+*
+* Return: Toujours 0
+*/
+int main(void)
+{
+	int nombre;
+
+	for (nombre = 0; nombre < 10; nombre++)
+	printf("%d", nombre);
+	printf("\n");
+
+return (0);
+}
