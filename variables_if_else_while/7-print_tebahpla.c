07274e825f93443401ebe6c5ef49cb8eb3c876eb
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-16 14:47:04 +0200

    ex7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main- alphabet a l'envers
+*
+* Return: Tjrs succes
+*
+*/
+int main(void)
+
+{
+write(1, "zyxwvutsrqponmlkjihgfedcba\n", 27);
+return (0);
+}
