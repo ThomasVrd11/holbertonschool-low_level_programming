4c4e70cd5fade4bf41cfcc6099a2b77ee5be84db
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-17 16:08:26 +0200

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - ajoute deux int.
+*
+* @num1: premier numero.
+*
+* @num2: deuxieme numero.
+*
+* Return: l'addition entre deux int.
+*
+*/
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
