d56b430659e14b8cc019227dce5d929506a7b990
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-19 20:57:36 +0200

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - ça fait une ligne 
+*
+* @n: le nombre de _ que je veux print
+*/
+void print_line(int n)
+{
+	int ligne;
+
+	if (n > 0)
+	{
+		for (ligne = 0; ligne < n; ligne++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
