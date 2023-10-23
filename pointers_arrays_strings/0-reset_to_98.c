551b04f917add88d36f5cf1ecc28f26f2cfb7c69
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-23 09:27:46 +0200

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* reset_to_98- fonction qui print un p a un int
+* et update la valeur que ça pointe a 98
+*
+* @n: est le pointeur int
+*
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
