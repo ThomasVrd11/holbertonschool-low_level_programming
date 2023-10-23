b3dc5f7b3185418838eda61f7041f82d4f3d8f59
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-23 11:16:11 +0200

    2

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -7,8 +7,8 @@
 */
 void swap_int(int *a, int *b)
 {
-	int c = *b;
-	int d = *a;
-	*a = c;
-	*b = d;
+	int c = *b;// Stocke la valeur de l'entier pointé par 'b' dans une variable temporaire 'c'.
+	int d = *a;// Stocke la valeur de l'entier pointé par 'a' dans une variable temporaire 'd'.
+	*a = c;// Affecte la valeur de 'c' à l'entier pointé par 'a', ce qui échange les valeurs de 'a' et 'b'.
+	*b = d;// Affecte la valeur de 'd' à l'entier pointé par 'b'.
 }
