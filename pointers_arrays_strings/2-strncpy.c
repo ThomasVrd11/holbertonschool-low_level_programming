dd0c8e6a4eb9f43eed4ada66da1c54b0670cc05e
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-24 15:18:55 +0200

    1

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strncpy - copie un string
+* @dest: la destination
+* @src: source
+* @n: le nombre de bytes venant de src
+* Return: le pointer vers dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+return (dest);
+}
