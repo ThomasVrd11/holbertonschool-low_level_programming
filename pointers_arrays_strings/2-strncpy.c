77f6e16ac7fa29a8fdcced145073749b68716b45
ThomasVrd11 <Thomasviard@orange.fr>
2023-10-24 15:32:56 +0200

    1

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
+// Boucle pour copier les caractères de src vers dest
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+// Si le nombre de caractères copiés est inférieur à n, remplir dest avec des caractères nuls ('\0')
 	for (; i < n; i++)
 	{
 		dest[i] = '\0';
 	}
+// Retourne un pointeur vers la chaîne de destination
 return (dest);
 }
---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare deux strings
+ * @s1: premier string.
+ * @s2: deuxieme string.
+ * Return: 0 si s1 et s2 sont egaux,
+ * un autre nombre si c'est pas le cas.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, stock = 0;
+
+	while (stock == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		stock = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (stock);
+}
