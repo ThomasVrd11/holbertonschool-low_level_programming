299184d5c5f4a4d923d8e6b2acb52f22338d9629
ThomasVrd11 <Thomasviard@orange.fr>
2023-11-03 20:23:42 +0100

    je push enfin

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,9 +7,11 @@
  * Return: 0 (success)
  */
 
-int main(int argc, char **argv) {
-    if (argc > 0) {
-        printf("%s\n", argv[0]);
-    }
-    return 0;
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
 }
