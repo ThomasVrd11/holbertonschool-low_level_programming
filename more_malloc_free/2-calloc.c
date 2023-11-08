01d6edc7c16f8a2ebc3ddd778e6f57c7d263da3d
ThomasVrd11 <Thomasviard@orange.fr>
2023-11-08 13:40:56 +0100

    jéaime pas le mercredi cest bon?

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - cacollac
+ * @nmemb: int 
+ * @size: taille
+ * Return: a pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p != NULL)
+		memset(p, 0, nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	else
+		return (p);
+}
