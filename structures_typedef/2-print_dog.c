8286969826e78e931ae3404746e2ea8eaeff6bf9
ThomasVrd11 <Thomasviard@orange.fr>
2023-11-15 08:52:31 +0100

    jai oublié age et changement de format

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,12 +11,17 @@ void print_dog(struct dog *d)
 	return;
 
 	if (d->name)
-		printf("Nom du chiengue: %s\n", d->name);
+		printf("Name: %s\n", d->name);
 	else
-		printf("Nom du chiengue: (nil)\n");
+		printf("Name: (nil)\n");
+
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)\n");
 
 	if (d->owner)
-		printf("Proprio: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	else
-		printf("Proprio: (nil)\n");
+		printf("Owner: (nil)\n");
 }
