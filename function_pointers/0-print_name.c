5993cee63ffccd1aaafb8eb6da8f7e2afe4fad26
ThomasVrd11 <Thomasviard@orange.fr>
2023-11-15 12:57:20 +0100

    test exercice 1 avec commentaires chelous

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -12,19 +12,3 @@ void print_name(char *name, void (*f)(char *))
 		f(name);
 }
 
-/**
- * @f: Function pointer to a function that takes a char pointer and
- *     returns nothing (void). This function is responsible for
- *     determining how the name is printed (e.g., as-is, uppercase, etc.).
- *
- * Description: This function takes a string and a function pointer as
- *     arguments. It checks if both the string and the function pointer
- *     are not null. If they are valid, it calls the function pointed to
- *     by 'f' and passes the name string to it. This design allows for
- *     flexibility in how the name is printed, as the printing behavior
- *     is determined by the function pointed to by 'f'.
- *     For instance, 'f' can point to a function that prints the name
- *     as-is, or another function that converts the name to uppercase
- *     before printing. This approach demonstrates the use of function
- *     pointers for callback functionality in C.
- */
