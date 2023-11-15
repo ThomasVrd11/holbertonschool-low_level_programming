9735f18500e0aef6a2998e733e5f8137aa701d20
ThomasVrd11 <Thomasviard@orange.fr>
2023-11-15 10:05:55 +0100

    ok alexandre ma explique mais meme lui avait des erreurs lol bouffon

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,24 +1,82 @@
 #include "dog.h"
 
 /**
+ * _strcpy - copie
+ * Description: copie
+ * @dest: destination
+ * @src: source
+ * Return: pointeur vers le string copié
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen - retourne la lenght
+ * Description: oe
+ * @str: str
+ * Return: str taille
+ */
+
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
+
+/**
 * new_dog - nouvelle regle nouveau chien
 * @name: nom chien
 * @age: j'en peux plus
 * @owner: tjrs la meme chose ptnnnn
-* Return: accouche 
+* Return: accouche
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p = malloc(sizeof(dog_t));
+	dog_t *patPatrouile;
 
-	if (p == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
+	return (NULL);
+
+	patPatrouile = malloc(sizeof(dog_t));
+	if (patPatrouile == NULL)
 	{
-		free(p);
-		return(NULL);
+		free(patPatrouile);
+		return (NULL);
 	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+	patPatrouile->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (patPatrouile->name == NULL)
+	{
+		free(patPatrouile->name);
+		free(patPatrouile);
+		return (NULL);
+	}
+	patPatrouile->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (patPatrouile->owner == NULL)
+	{
+		free(patPatrouile->name);
+		free(patPatrouile->owner);
+		free(patPatrouile);
+		return (NULL);
+	}
+
+	patPatrouile->name = _strcpy(patPatrouile->name, name);
+	patPatrouile->age = age;
+	patPatrouile->owner = _strcpy(patPatrouile->owner, owner);
+
+	return (patPatrouile);
 }
