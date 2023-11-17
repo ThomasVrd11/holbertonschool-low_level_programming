83170bdf750f8603705dcc1dd48a0e0eb0c415b7
ThomasVrd11 <Thomasviard@orange.fr>
2023-11-17 11:08:43 +0100

    ca passe ou bien

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - ilo est ou le int ???,
+ *@array: le paramaetre 
+ *@size: taille array
+ *@cmp: pointeur 
+ *
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != 0 && size != 0 && cmp != 0)
+		for (i = 0; i < size; i++)
+		{
+			if ((cmp)(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	return (-1);
+}
