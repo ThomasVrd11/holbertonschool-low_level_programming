4f2e98e0a7cf84a585fcb4af06fb70fe658f5940
ThomasVrd11 <Thomasviard@orange.fr>
2024-01-19 19:35:05 +0100

    0 was wrong

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,18 +12,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
-	while (i < size)
+
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (value == array[i])
-		{
+		if (array[i] == value)
 			return (i);
-		}
-		i++;
 	}
 	return (-1);
 }
